1bcab12fa8289932da63bc7af27b6b83be683b9b
MaheelLakshan <MaheelLakshanra@gmail.com>
2024-09-14 16:12:55 +0530

    cache the credentials

--- App.js
+++ App.js
@@ -2,10 +2,28 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './App/Screens/LoginScreen/Login';
 import { ClerkProvider, SignedIn, SignedOut, useUser } from '@clerk/clerk-expo';
+import * as SecureStore from 'expo-secure-store';
+
+const tokenCache = {
+  async saveToken(key, value) {
+    let res = await SecureStore.setItemAsync(key, value);
+    return res;
+  },
+
+  async getToken(key) {
+    let result = await SecureStore.getItemAsync(key);
+    if (result) {
+      return result;
+      // alert("🔐 Here's your value 🔐 \n" + result);
+    } else {
+      alert('No values stored under that key.');
+    }
+  },
+};
 
 export default function App() {
   return (
-    <ClerkProvider publishableKey="pk_test_Z2VudGxlLW11dHQtNzkuY2xlcmsuYWNjb3VudHMuZGV2JA">
+    <ClerkProvider tokenCache={tokenCache} publishableKey="pk_test_Z2VudGxlLW11dHQtNzkuY2xlcmsuYWNjb3VudHMuZGV2JA">
       <View style={styles.container}>
         <SignedIn>
           <UserDetails />
